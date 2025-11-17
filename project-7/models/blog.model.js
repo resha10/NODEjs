7f6c90bb4939d4a02c5f598a64a95f01dcb8d26a
resha nakrani <reshanakrani@gmail.com>
2025-11-17 05:58:53 +0530

    project-7

---
+++ project-7/models/blog.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  content: String,
+  author: String,
+  category: String,
+  language: String,
+  date: Date,
+  image: String,
+  createdAt: { type: Date, default: Date.now },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" } 
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+module.exports = Blog;
